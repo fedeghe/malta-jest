c5e037b724ea08a50a766a6bb3265b23222ab748
fedeghe <fedeghe@gmail.com>
2025-06-05 23:11:32 +0200

    errs

--- index.js
+++ index.js
@@ -5,7 +5,9 @@ function maltaJest(obj, options) {
         start = new Date(),
         pluginName = path.basename(path.dirname(__filename)),
         targetFile = this.data.name;
-    let msg;
+    let msg,
+        output = '',
+        errorOutput = '';
     options = options || {};
     return (solve, reject) => {
         const next = () => {
@@ -13,14 +15,27 @@ function maltaJest(obj, options) {
                 self.notifyAndUnlock(start, msg);
             },
             jest = cp.exec(`"jest" ${options?.argz}`);
+        
         console.log(`${pluginName}: ${targetFile}`)
         jest.stdout.on("data", data => {
-            console.log(data);
+            output += data;
             next();
         });
         jest.stderr.on("data", data => {
-            console.log(data);
-            next();
+            errorOutput += data;
+        });
+        jest.on("close", code => {
+            if (code === 0) {
+                console.log(output);
+                solve(obj);
+                self.notifyAndUnlock(start, msg);
+            } else {
+                console.log("ERROR".red());
+                msg = 'plugin ' + pluginName.white() + ' compilation error';
+                console.log((output + errorOutput).white());
+                reject(`ERROR: some tests are failing\nplugin ${pluginName}: SOME TESTS ARE FAILING`);
+                self.notifyAndUnlock(start, msg);
+            }
         });
     }
 }
